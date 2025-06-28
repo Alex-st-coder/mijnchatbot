7b6ee3aaa98ec1a369d71f5019206fd7b8110329
testbotvanalex <testbotvanalex@gmail.com>
2025-06-28 13:09:10 +0200

    Обновлен файл index.js с фиксом для Puppeteer

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const OpenAI = require('openai');
 
-// ✅ Новый способ инициализации OpenAI
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const client = new Client();
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+});
 
 client.on('qr', qr => {
   qrcode.generate(qr, { small: true });
